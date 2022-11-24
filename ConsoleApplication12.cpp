cb054adc5e99112b2f2ed4e0514c83d73760e428
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-11-24 16:56:24 +0300

    Add files via upload

---
+++ ConsoleApplication12.cpp
@@ -0,0 +1,19 @@
+﻿
+//Чертоусова телефон дз
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    int a, b, c, t;
+    cin >> a >> b >> c >> t;
+    int s = 0;
+    if (t <= a) cout << t * b;
+    else {
+        s = a * b + (t - a) * c;
+        cout << s;
+    }
+    
+}
+
